afac1fa0a78befefbb98de91db321f2a0f591fab
Hassaam Mehtab <h455a4mmehtab@gmail.com>
2023-03-16 21:15:34 +0500

    Website Completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
-
+import Service from "./components/Services";
 import './style/App.css';
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/contact" element={<Contact/>} />
+                <Route path="/services" element={<Service/>} />
             </Routes>
             <Footer/>
         </Router>
---
+++ src/components/Services.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import img1 from '../assets/3.jpg'
+import img2 from '../assets/4.png'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const Services = () => {
+    return (
+        <div className="services">
+            <Carousel infiniteLoop showStatus={false} autoPlay interval={1000} showThumbs={false} showArrows={false}>
+                <div>
+                    <img src={img1} alt="Item1" />
+                    <p className="legend">Full Stack</p>
+                </div>
+                <div>
+                    <img src={img2} alt="Item2" />
+                    <p className="legend">Peer-to-peer Support</p>
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Services;
